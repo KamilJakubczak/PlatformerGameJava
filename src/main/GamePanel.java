a4e24b91d6e8e97d0928374157238cf49cd7ee72
kamiljakubczak <kamiljakubczak@outlook.com>
2023-03-09 14:07:56 +0100

    Added better game loop and some images

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,41 +3,70 @@ package main;
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
-    private int xDelta =100, yDelta =100;
+    private float xDelta = 100, yDelta = 100;
+    private BufferedImage img, subImg;
+
+
     public GamePanel() {
         mouseInputs = new MouseInputs(this);
+
+        importImg();
+
+        setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
     }
 
+    private void importImg() {
+        InputStream is = getClass().getResourceAsStream("/player_sprites.png");
+        try {
+            img = ImageIO.read(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280, 800);
+        setMinimumSize(size);
+        setPreferredSize(size);
+        setMinimumSize(size);
+    }
+
     public void changeXDelta(int value) {
         this.xDelta += value;
-        repaint();
     }
+
     public void changeYDelta(int value) {
         this.yDelta += value;
-        repaint();
     }
 
     public void setRectPos(int x, int y) {
         this.xDelta = x;
         this.yDelta = y;
-        repaint();
 
     }
-
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.fillRect(xDelta, yDelta,200, 50);
+
+        subImg = img.getSubimage(1*64,8*40,64,40);
+        g.drawImage(subImg, (int)xDelta,(int)yDelta,124,80,null);
+
+        //Fix lagging without mouse move some architecture issue
+        Toolkit.getDefaultToolkit().sync();
     }
+
 }
